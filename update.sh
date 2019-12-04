0afa5f08faadcd1a294f28e3f9f1abfbd0c3ae6b
Thomas Zakrajsek <tzakras@linux.com>
2019-12-04 04:22:06 -0800

    updated for new decade

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 #Authored by: Thomas Zakrajsek <tzakrajs@linux.com>
 
 # Globals
-SHARED_IP=24.130.214.172
+SHARED_IP=75.3.240.17
 LE_SERVER=https://acme-v01.api.letsencrypt.org/directory
 #LE_SERVER=https://acme-staging.api.letsencrypt.org/directory
 RUN_FILE=/var/run/cloud-fortress-lets-encrypt.run
@@ -15,7 +15,9 @@ if [[ ! -f $RUN_FILE ]]; then
 else
   if [[ "$(echo $current_md5)" == "$(cat $RUN_FILE)" ]]; then
       echo "No changes detected..."
-      exit
+      if sys.argv[1] != '-f'; then
+          exit
+      fi
   fi
   echo "Changes detected, updating SSL certificate!"
 fi
@@ -28,16 +30,20 @@ for vhost in $(find -L /etc/apache2/sites-enabled/ -mmin -2) $(find /etc/apache2
   if [[ $vhost == "*le-ssl*" ]]; then
     continue
   fi
+  if [[ $vhost == "*/" ]]; then
+    continue
+  fi
   domains=""
   for domain in $(grep -Ei '(ServerName|ServerAlias)' $vhost|awk '{print $2}'); do
     if [[ "`dig @8.8.8.8 +short +time=1 +tries=2 $domain|tail -1`" == "$SHARED_IP" ]]; then
       domains="$domains-d $domain "
     fi 
   done
+  echo $domains
   if [[ "$domains" == "" ]]; then
     continue
   fi
-  /opt/letsencrypt/letsencrypt-auto --break-my-certs --server $LE_SERVER --renew-by-default --standalone --standalone-supported-challenges http-01 --http-01-port 9999 --email sysadmin@cloud-fortress.com --text --agree-tos $domains run --installer apache
+  certbot --break-my-certs --server $LE_SERVER --renew-by-default --apache --email sysadmin@cloud-fortress.com --text --agree-tos $domains
   touch -h $vhost
 done
 
